8bef4cefb557d1ae422f774b02bbf6afb8fc4b54
olaoluwa <adeolarhowolabi@gmail.com>
2023-02-16 17:03:32 -0800

    comment

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 /*string initialisation*/
-char mystr[] = "\"Programming is like building a multilingual puzzle";    
+char mystr[] = "\"Programming is like building a multilingual puzzle";
 puts(mystr);
 return (0);
 }
