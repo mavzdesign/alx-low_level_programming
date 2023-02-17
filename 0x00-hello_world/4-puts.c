481248ce394815b32e8572fb6d8ddfda7716bcc0
olaoluwa <adeolarhowolabi@gmail.com>
2023-02-16 16:53:44 -0800

    betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,7 @@
 int main(void)
 {
 /*string initialisation*/
-char mystr[] = "\"Programming is like building a multilingual puzzle";
-    
+char mystr[] = "\"Programming is like building a multilingual puzzle";    
 puts(mystr);
 return (0);
 }
