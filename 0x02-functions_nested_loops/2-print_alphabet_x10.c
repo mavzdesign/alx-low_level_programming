9d944b163a54e4dbb5b651517b5713d60aa48df9
olaoluwa adewunmi <adeolarhowolabi@gmail.com>
2023-02-21 12:08:10 -0800

    x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print lowercase alphabet ten times
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+int counter;
+
+for (counter = 1; counter <= 10; counter++)
+{
+
+print_alphabet();
+_putchar('\n');
+
+}
+
+}
