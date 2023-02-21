7533d7723378e66c2e5e606717d34280533f7217
olaoluwa adewunmi <adeolarhowolabi@gmail.com>
2023-02-21 12:55:44 -0800

    alphabetx10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,14 +7,15 @@
 
 void print_alphabet_x10(void)
 {
-int counter;
+	char alpha;
+	int counter;
 
-for (counter = 1; counter <= 10; counter++)
-{
-
-print_alphabet();
-_putchar('\n');
-
-}
-
+	for (counter = 1; counter <= 10; counter++)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+		_putchar('\n');
+	}
 }
