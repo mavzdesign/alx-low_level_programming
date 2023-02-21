c35f4592a74edb147d37c0555d4f78f1a8fbaeba
olaoluwa adewunmi <adeolarhowolabi@gmail.com>
2023-02-21 08:46:06 -0800

    first trial

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+char text [] = "putchar";
+  
+_putchar(text);
+
+return (0);
+
+}
