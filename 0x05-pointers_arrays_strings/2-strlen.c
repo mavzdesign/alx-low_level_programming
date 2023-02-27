15cfcb4e20b2ca20d033e9f1d2fa437aa57a97fe
olaoluwa adewunmi <adeolarhowolabi@gmail.com>
2023-02-27 13:21:46 -0800

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * _strlen - Returns the length of a string
+  * @s: String to count
+  *
+  * Return: String length
+  */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; *s != '\0'; s++)
+	{
+		i++;
+	}
+
+	return (i);
+}
